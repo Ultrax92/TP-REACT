81c3025117e09c0db13530635b26ce29eb5103b8
Ultrax92 <kiwiihs@gmail.com>
2025-03-03 14:42:41 +0100

    Fix ACTIVITE 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
 import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>
       <Header />
-    </div>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
-
 const Header = () => {
     return (
         <header>
             <img
                 src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
                 alt="Logo"
-                style={{ width: '100px', height: 'auto' }}
             />
 
             <nav>
