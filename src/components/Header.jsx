4f93a2e458852477833e880f40a58a89a1b473c5
Ultrax92 <kiwiihs@gmail.com>
2025-03-03 14:35:28 +0100

    ACTIVITE 4

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <img
+                src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
+                alt="Logo"
+                style={{ width: '100px', height: 'auto' }}
+            />
+
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/about">À propos</a>
+                    </li>
+                    <li>
+                        <a href="/contact">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
