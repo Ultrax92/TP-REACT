4a616680b40a0dc6c52a2cca072607bcdeecfcfe
Ultrax92 <kiwiihs@gmail.com>
2025-03-04 16:04:45 +0100

    Activité 9

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,10 +1,26 @@
-import Card from "react-bootstrap/Card";
+import { Card, Badge } from "react-bootstrap";
 import "../assets/styles/dish.scss";
 
-function Dish({ name, price, imgSrc }) {
+function Dish({ name, price, imgSrc, isNew }) {
   return (
     <Card>
-      <Card.Img src={imgSrc} />
+      <div style={{ position: "relative" }}>
+        {isNew && (
+          <Badge
+            pill
+            variant="success"
+            style={{
+              position: "absolute",
+              top: "10px",
+              right: "10px",
+              zIndex: 10,
+            }}
+          >
+            Nouveau
+          </Badge>
+        )}
+        <Card.Img src={imgSrc} />
+      </div>
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>Prix : {price}€</Card.Text>
