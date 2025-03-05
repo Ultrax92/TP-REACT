f73716547807235da308e05090f4d03cd196d82c
Ultrax92 <kiwiihs@gmail.com>
2025-03-05 10:00:31 +0100

    Bug fix Activité 7

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <main>
         <Container>
           <Row>
-            <Col md={3}>
+            <Col md={4}>
               <Dish
                 name="Tacos à l'unité"
                 description="Savourez l'authenticité mexicaine avec notre délicieux tacos à l'unité. Garni de viande grillée, de légumes frais et de sauce maison, chaque bouchée vous transporte directement au cœur du Mexique."
@@ -20,7 +20,7 @@ function App() {
                 isNew={true}
               />
             </Col>
-            <Col md={3}>
+            <Col md={4}>
               <Dish
                 name="Enchiladas"
                 description="Nos enchiladas sont un festin de saveurs, avec des tortillas de maïs enroulées autour d'une garniture généreuse de viande, de fromage fondu et de sauce tomate épicée. Chaque bouchée est une explosion de goût qui vous fera revenir pour plus."
@@ -29,7 +29,7 @@ function App() {
                 isNew={false}
               />
             </Col>
-            <Col md={3}>
+            <Col md={4}>
               <Dish
                 name="Mole poblano"
                 description="Découvrez la richesse de la cuisine mexicaine avec notre mole poblano. Cette sauce complexe et riche en saveurs est préparée avec une combinaison d'épices, de chocolat et de piments, offrant une expérience culinaire unique et exquise."
