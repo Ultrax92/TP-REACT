f9275497ed3ba6b0f654cfb9a7999d72ef0a3ef1
Ultrax92 <kiwiihs@gmail.com>
2025-03-05 15:36:03 +0100

    Activité 12

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,18 +1,20 @@
-import { Card, Badge } from "react-bootstrap";
+import { Card, Badge, Button } from "react-bootstrap";
 import "../assets/styles/dish.scss";
 
 function Dish({ name, price, imgSrc, isNew }) {
+  const handleAddToCart = () => {
+    alert(`Le plat ${name} est maintenant dans votre panier`);
+  };
   return (
     <Card>
-      {isNew && (
-        <Badge variant="success">
-          Nouveau
-        </Badge>
-      )}
+      {isNew && <Badge variant="success">Nouveau</Badge>}
       <Card.Img src={imgSrc} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>Prix : {price}€</Card.Text>
+        <Button variant="primary" onClick={handleAddToCart}>
+          Ajouter au panier
+        </Button>
       </Card.Body>
     </Card>
   );
