caaf9ff1f2dc81e43d6a795322203e26ec4e8377
Ultrax92 <kiwiihs@gmail.com>
2025-03-05 14:12:17 +0100

    Activité 10

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,61 @@ import "./App.scss";
 import Dish from "./components/Dish";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Home from "./components/Home";
 
 function App() {
+  const dishes = [
+    {
+      id: 1,
+      name: "Tacos à l'unité",
+      description:
+        "Savourez l'authenticité mexicaine avec notre délicieux tacos à l'unité. Garni de viande grillée, de légumes frais et de sauce maison, chaque bouchée vous transporte directement au cœur du Mexique.",
+      price: 3,
+      imgSrc:
+        "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+      isNew: true,
+    },
+    {
+      id: 2,
+      name: "Enchiladas",
+      description:
+        "Nos enchiladas sont un festin de saveurs, avec des tortillas de maïs enroulées autour d'une garniture généreuse de viande, de fromage fondu et de sauce tomate épicée. Chaque bouchée est une explosion de goût qui vous fera revenir pour plus.",
+      price: 12,
+      imgSrc:
+        "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+      isNew: false,
+    },
+    {
+      id: 3,
+      name: "Mole poblano",
+      description:
+        "Découvrez la richesse de la cuisine mexicaine avec notre mole poblano. Cette sauce complexe et riche en saveurs est préparée avec une combinaison d'épices, de chocolat et de piments, offrant une expérience culinaire unique et exquise.",
+      price: 3,
+      imgSrc:
+        "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+      isNew: false,
+    },
+  ];
+
   return (
     <>
       <Header />
       <main>
         <Container>
           <Row>
-            <Col md={4}>
-              <Dish
-                name="Tacos à l'unité"
-                description="Savourez l'authenticité mexicaine avec notre délicieux tacos à l'unité. Garni de viande grillée, de légumes frais et de sauce maison, chaque bouchée vous transporte directement au cœur du Mexique."
-                price={3}
-                imgSrc="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-                isNew={true}
-              />
-            </Col>
-            <Col md={4}>
-              <Dish
-                name="Enchiladas"
-                description="Nos enchiladas sont un festin de saveurs, avec des tortillas de maïs enroulées autour d'une garniture généreuse de viande, de fromage fondu et de sauce tomate épicée. Chaque bouchée est une explosion de goût qui vous fera revenir pour plus."
-                price={12}
-                imgSrc="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-                isNew={false}
-              />
-            </Col>
-            <Col md={4}>
-              <Dish
-                name="Mole poblano"
-                description="Découvrez la richesse de la cuisine mexicaine avec notre mole poblano. Cette sauce complexe et riche en saveurs est préparée avec une combinaison d'épices, de chocolat et de piments, offrant une expérience culinaire unique et exquise."
-                price={3}
-                imgSrc="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-                isNew={false}
-              />
-            </Col>
+            {dishes.map((dish) => (
+              <Col key={dish.id} md={4}>
+                <Dish
+                  name={dish.name}
+                  price={dish.price}
+                  imgSrc={dish.imgSrc}
+                  isNew={dish.isNew}
+                />
+              </Col>
+            ))}
           </Row>
         </Container>
       </main>
       <Footer />
-      <Home />
     </>
   );
 }
