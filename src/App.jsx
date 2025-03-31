5dea15ee92f28f4891967117def5d9d530cd36b8
Ultrax92 <kiwiihs@gmail.com>
2025-03-31 16:58:50 +0200

    Activité 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import "./App.css";
 
 function App() {
@@ -14,11 +14,35 @@ function App() {
     fetchProducts();
   }, []);
 
+  async function handleAddProduct() {
+    const response = await fetch("https://fakestoreapi.com/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "Nouveau produit",
+        price: 29.99,
+        description: "Produit ajouté via l'app React",
+        image: "https://via.placeholder.com/150",
+        category: "electronics",
+      }),
+    });
+
+    const data = await response.json();
+    alert(`Le produit avec l'id ${data.id} a été créé`);
+  }
+
   return (
     <Container className="my-4">
+      <div className="btnProduct mb-3">
+      <Button variant="primary" onClick={handleAddProduct}>
+          Ajouter un produit
+        </Button>
+      </div>
       <Row className="g-4">
         {products.map((product) => (
-          <Col xs={1} sm={2} md={3} lg={3} key={product.id}>
+          <Col xs={12} sm={6} md={6} lg={4} xl={3} key={product.id}>
             <Card className="h-100">
               <Card.Img
                 variant="top"
